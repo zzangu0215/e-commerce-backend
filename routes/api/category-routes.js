13ad80180fea702bd1ab060e639a42917fdf4511
Junny <tajo0215@gmail.com>
2021-07-24 09:06:32 -0700

    put /api/categories/:id route implemented

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,7 +25,7 @@ router.get("/:id", async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: "No category found with this id! "});
+      res.status(404).json({ message: "No category found with this id! " });
       return;
     }
 
@@ -45,8 +45,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "No category found with this id! "});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {}
 });
 
 router.delete("/:id", (req, res) => {
