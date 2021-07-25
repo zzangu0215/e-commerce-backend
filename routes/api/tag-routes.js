e4ad031b4aafe87039e59eca8a1efa2957f5052f
Junny <tajo0215@gmail.com>
2021-07-24 21:01:02 -0700

    DELETE /api/tags/:id route implemented

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,8 +35,13 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  // try {
+  //   const tagData = await Product.create(req.body);
+
+  //   if (req.body.tagIds.leng)
+  // } catch (err) {}
 });
 
 router.put("/:id", (req, res) => {
@@ -45,6 +50,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
