6acea72764899c5b4e3ca98692c7072322d1e399
Junny <tajo0215@gmail.com>
2021-07-24 20:14:29 -0700

    GET request for /api/tags/:id route implemented

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No Tag found with this id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
