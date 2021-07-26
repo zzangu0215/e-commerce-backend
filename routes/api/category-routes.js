9f1f1440382e1f488a204033c3982a339f783ba9
Junny <tajo0215@gmail.com>
2021-07-26 09:30:44 -0700

    all routes implemented correctly

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,6 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
@@ -17,8 +15,6 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
@@ -36,36 +32,15 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
-
-  // try {
-  //   const categoryData = await Category.create(req.body);
-
-  //   console.log(req.body.productIds.length);
-  //   if (req.body.productIds.length) {
-  //     const categoryProductArr = req.body.productIds.map((product_id) => {
-  //       return {
-  //         category_id: categoryData.id,
-  //         product_id,
-  //       };
-  //     });
-  //     return Product.create(categoryProductArr);
-  //   }
-
-  //   res.status(200).json(categoryData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
 });
 
 router.put("/:id", async (req, res) => {
-  // update a category by its `id` value
   try {
     const categoryData = await Category.update(req.body, {
       where: {
@@ -85,7 +60,6 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: {
