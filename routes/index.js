1cf76b767f6044e28450b387c9ea2fcdd00578ab
zzangu0215 <tajo0215@gmail.com>
2021-07-26 17:15:26 -0700

    home route changed for a little bit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send('<h1>Wrong Route!</h1>');
+  res.send(`<h1>Wrong Route!</h1>
+  <h2>Move on to the following routes!</h2>
+  <h3>/api/categories</h3>
+  <h3>/api/products</h3>
+  <h3>/api/tags</h3>`);
 });
 
 module.exports = router;
